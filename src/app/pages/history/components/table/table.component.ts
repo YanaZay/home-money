7788dcf246e129f063bf79e719279437e9c7ed1c
Viacheslav Zaitsev <viacheslav.zajcev@gmail.com>
2021-10-27 10:10:11 +0300

    27/10/2021 eliminate minor errors in code

--- src/app/pages/history/components/table/table.component.ts
+++ src/app/pages/history/components/table/table.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { IEvents } from '../../../../shared/models/events.interface';
 import { HistoryService } from '../../history.service';
 import { ICategories } from '../../../../shared/models/categories.interface';
+import {MatTableDataSource} from "@angular/material/table";
 
 @Component({
   selector: 'app-table',
@@ -9,31 +10,35 @@ import { ICategories } from '../../../../shared/models/categories.interface';
   styleUrls: ['./table.component.scss']
 })
 export class TableComponent implements OnInit {
-  public currentData: IEvents[] = [];
+  public currentData!: MatTableDataSource<IEvents>;
   public displayedColumns: string[] = ['id', 'amount', 'date', 'category', 'type', 'action'];
-  public typeStyle!: boolean;
 
   constructor(
     private historyService: HistoryService
   ) {}
 
   public ngOnInit(): void {
+    let currentValue: IEvents[] = [];
     this.historyService.getEvents().subscribe((events:IEvents[]) => {
-      this.currentData = events;
+      currentValue = events;
+
+      this.currentData = new MatTableDataSource(events);
     });
 
     this.historyService.getCategories().subscribe((category:ICategories[]) => {
       for (let cat of category) {
-        for (let event of this.currentData) {
+        for (let event of currentValue) {
           cat.id === event.category ? event.category = cat.name : null;
         }
       }
-      console.log(this.currentData)
     })
+    console.log(this.currentData)
+    console.log(currentValue)
+
   }
-  // public applyFilter(event: Event) {
-  //   const filterValue = (event.target as HTMLInputElement).value;
-  //   this.dataSource.filter = filterValue.trim().toLowerCase();
-  // }
+  public applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.currentData.filter = filterValue.trim().toLowerCase();
+  }
 }
 
