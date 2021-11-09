bc65bb0f32baa1897cf07fdcb3d05e40954cefc1
YanaZay <yanakipot@gmail.com>
2021-11-09 12:55:27 +0200

    change setTimeout on timer in login.component

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -36,7 +36,7 @@ export class LoginComponent implements OnInit, OnDestroy {
       this.done = !!params.done;
     });
     timer(3000).subscribe( () => {
-      this.done = false
+      this.done = false;
     });
   }
 
