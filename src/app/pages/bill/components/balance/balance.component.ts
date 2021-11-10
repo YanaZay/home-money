26440e4aebce2851beb151b5a0d0f9c1a01055e5
Viacheslav Zaitsev <viacheslav.zajcev@gmail.com>
2021-11-10 17:28:19 +0200

    some check

--- src/app/pages/bill/components/balance/balance.component.ts
+++ src/app/pages/bill/components/balance/balance.component.ts
@@ -1,6 +1,9 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { IBill } from "../../../../shared/models/bill.interface";
+import { Subject } from "rxjs";
+import { takeUntil } from "rxjs/operators";
 import { IExchangeInterface } from '../../../../shared/models/exchange.interface';
+import {BillService} from "../../bill.service";
 
 @Component({
   selector: 'app-balance',
@@ -8,7 +11,23 @@ import { IExchangeInterface } from '../../../../shared/models/exchange.interface
   styleUrls: ['./balance.component.scss']
 })
 
-export class BalanceComponent {
+export class BalanceComponent implements OnInit, OnDestroy {
   @Input() public dataRates!: IExchangeInterface;
-  @Input() public dataBill!: IBill;
+  public bill!: number;
+  private destroy$: Subject<void> = new Subject<void>();
+
+  constructor(private billService: BillService) {}
+
+  public ngOnInit(): void {
+    this.billService.getBill()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((data:IBill) => {
+        this.bill = data.value;
+      })
+  }
+
+  public ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
