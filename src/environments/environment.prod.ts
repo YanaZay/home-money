922fae779a5875d6737ea988d4b5990415def344
YanaZay <yanakipot@gmail.com>
2021-11-11 17:24:13 +0200

    some check

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiHost: 'http://localhost:3030',
+  exchangeRates: 'http://data.fixer.io/api/latest?access_key=156f43c852d2eb2cdca7a4ba965e720a'
 };
