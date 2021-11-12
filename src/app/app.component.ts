54502f7e15f809e88a31e73626ce1ac6b0d3147f
YanaZay <yanakipot@gmail.com>
2021-11-12 16:04:44 +0200

    edit package.json and angular.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, isDevMode, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'home-money';
+export class AppComponent implements OnInit {
+  public title = 'home-money';
+
+  public ngOnInit(): void {
+    if (isDevMode()) {
+      console.log('Development!');
+    } else {
+      console.log('Production!');
+    }
+  }
 }
