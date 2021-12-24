068a12c2bc88021dc905d5311e9d4e4119d1e3c5
YanaZay <yanakipot@gmail.com>
2021-12-24 17:02:45 +0200

    install prettier

--- src/app/pages/auth/store/effects/register.effect.ts
+++ src/app/pages/auth/store/effects/register.effect.ts
@@ -1,42 +1,35 @@
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
 
-import { Actions, createEffect, ofType } from "@ngrx/effects";
+import { Actions, createEffect, ofType } from '@ngrx/effects';
 
-import { switchMap, map, catchError } from "rxjs/operators";
+import { switchMap, map, catchError } from 'rxjs/operators';
 import { of } from 'rxjs';
 
 import {
   registerAction,
   registerFailureAction,
-  registerSuccessAction} from "../actions/register.action";
-import { AuthService } from "../../auth.service";
-import { IUserResponse } from "../../../../shared/models/userResponse.interface";
+  registerSuccessAction,
+} from '../actions/register.action';
+import { AuthService } from '../../auth.service';
+import { IUserResponse } from '../../../../shared/models/userResponse.interface';
 
 @Injectable()
 export class RegisterEffect {
+  private register$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(registerAction),
+      switchMap(({ request }) => {
+        return this.AuthService.register(request).pipe(
+          map((currentUser: IUserResponse) => {
+            return registerSuccessAction({ currentUser });
+          }),
+          catchError(() => {
+            return of(registerFailureAction);
+          })
+        );
+      })
+    )
+  );
 
-  private register$ = createEffect(
-    () => this.actions$
-      .pipe(
-        ofType(registerAction),
-        switchMap(
-          ({request}) => {
-            return this.AuthService.register(request)
-              .pipe(
-                map((currentUser: IUserResponse) => {
-                  return registerSuccessAction({currentUser})
-                }),
-                catchError(() => {
-                  return of(registerFailureAction)
-                })
-              )
-          }
-        )
-      )
-  )
-
-  constructor(
-    private actions$: Actions,
-    private AuthService: AuthService
-  ) {}
+  constructor(private actions$: Actions, private AuthService: AuthService) {}
 }
