f400c42e7bf53004fdbe32686e68ab43d4d2d03d
YanaZay <yanakipot@gmail.com>
2021-12-24 12:41:07 +0200

    add new actions for regist component

--- src/app/pages/auth/store/actions/register.action.ts
+++ src/app/pages/auth/store/actions/register.action.ts
@@ -1,8 +1,18 @@
 import { createAction, props } from "@ngrx/store";
 
 import { IUserRequest } from "../../../../shared/models/userRequest.interface";
+import { IUserResponse } from "../../../../shared/models/userResponse.interface";
 
 export const registerAction = createAction(
   '[Auth] Register',
-  props<{ user: IUserRequest }>()
+  props<{ request: IUserRequest }>()
+)
+
+export const registerSuccessAction = createAction(
+  '[Auth] Register success',
+  props<{ currentUser: IUserResponse }>()
+)
+
+export const registerFailureAction = createAction(
+  '[Auth] Register failure',
 )
