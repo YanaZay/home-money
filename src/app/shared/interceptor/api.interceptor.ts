bb3847da03071a2287f0f66e0e663cfcad04c701
YanaZay <yanakipot@gmail.com>
2022-01-05 15:41:56 +0200

    updated api interceptor

--- src/app/shared/interceptor/api.interceptor.ts
+++ src/app/shared/interceptor/api.interceptor.ts
@@ -3,16 +3,21 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
 
 @Injectable()
 export class ApiInterceptor implements HttpInterceptor {
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const newUrl = {url: `${environment.apiHost}`};
-    request = Object.assign(request, newUrl);
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    request = request.clone({
+      url: environment.apiHost + request.url,
+    });
+
     return next.handle(request);
   }
 }
