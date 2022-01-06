e8f8c3f59249086264dec9a4c48a63a3772ffeb2
YanaZay <yanakipot@gmail.com>
2022-01-06 16:55:17 +0200

    fixed bag in bill (after added interceptor)

--- src/app/pages/bill/components/balance/balance.component.ts
+++ src/app/pages/bill/components/balance/balance.component.ts
@@ -1,17 +1,15 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
-import { environment } from "../../../../../environments/environment";
-import { IBill } from "../../../../shared/models/bill.interface";
-import { Subject } from "rxjs";
-import { takeUntil } from "rxjs/operators";
+import { HttpClient } from '@angular/common/http';
+import { IBill } from '../../../../shared/models/bill.interface';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 import { IExchangeInterface } from '../../../../shared/models/exchange.interface';
 
 @Component({
   selector: 'app-balance',
   templateUrl: './balance.component.html',
-  styleUrls: ['./balance.component.scss']
+  styleUrls: ['./balance.component.scss'],
 })
-
 export class BalanceComponent implements OnInit, OnDestroy {
   @Input() public dataRates!: IExchangeInterface;
   public bill!: number;
@@ -20,11 +18,12 @@ export class BalanceComponent implements OnInit, OnDestroy {
   constructor(private http: HttpClient) {}
 
   public ngOnInit(): void {
-    this.http.get<IBill>(`${environment.apiHost}/bill`)
+    this.http
+      .get<IBill>('/bill')
       .pipe(takeUntil(this.destroy$))
-      .subscribe((data:IBill) => {
-      this.bill = data.value;
-    })
+      .subscribe((data: IBill) => {
+        this.bill = data.value;
+      });
   }
 
   public ngOnDestroy(): void {
